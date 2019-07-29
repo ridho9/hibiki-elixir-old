d2792ac11a259c6ea29406d614381393c563a2d8
Ridho Pratama <p.ridho@yahoo.co.id>
2019-07-29 22:59:39 +0700

    Refactor context

--- apps/hibiki/lib/hibiki/context.ex
+++ apps/hibiki/lib/hibiki/context.ex
@@ -23,30 +23,34 @@ defmodule Hibiki.Context do
 
   def set_command(ctx, cmd), do: %{ctx | command: cmd}
 
-  def add_text_message(%Context{messages: messages} = ctx, text) do
-    messages = messages ++ [%{"type" => "text", "text" => text}]
-    %{ctx | messages: messages}
+  def add_text_message(ctx, text) do
+    text_messages = %{"type" => "text", "text" => text}
+    add_message(ctx, text_messages)
   end
 
   def add_image_message(ctx, original_url) do
     add_image_message(ctx, original_url, original_url)
   end
 
-  def add_image_message(%Context{messages: messages} = ctx, original_url, preview_url) do
+  def add_image_message(ctx, original_url, preview_url) do
     image_message = %{
       "type" => "image",
       "original_content_url" => original_url,
       "preview_image_url" => preview_url
     }
 
-    messages = messages ++ [image_message]
-    %{ctx | messages: messages}
+    add_message(ctx, image_message)
+  end
+
+  def add_message(%Context{messages: messages} = ctx, message) do
+    %{ctx | messages: messages ++ [message]}
   end
 
   def send_reply(
         %Context{client: client, messages: messages, event: %{"reply_token" => reply_token}} = ctx
       ) do
     LineSDK.Client.send_reply(client, reply_token, messages)
+
     %{ctx | replied: true}
   end
 end
