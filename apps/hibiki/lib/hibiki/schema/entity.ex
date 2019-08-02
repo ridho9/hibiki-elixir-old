482bc8926605cd9c198fd762a4cd7c54f585aa93
Ridho Pratama <p.ridho@yahoo.co.id>
2019-08-02 23:41:50 +0700

    Added global entity selector

--- apps/hibiki/lib/hibiki/schema/entity.ex
+++ apps/hibiki/lib/hibiki/schema/entity.ex
@@ -8,4 +8,8 @@ defmodule Hibiki.Schema.Entity do
     has_many(:created_tags, Hibiki.Schema.Tag, foreign_key: :creator_id)
     has_many(:scope_tags, Hibiki.Schema.Tag, foreign_key: :scope_id)
   end
+
+  def global() do
+    Hibiki.Repo.get(__MODULE__, 1)
+  end
 end
