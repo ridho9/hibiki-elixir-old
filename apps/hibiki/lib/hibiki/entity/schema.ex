8b12d776ff913f728f8e62714edac51d26ab135c
Ridho Pratama <p.ridho@yahoo.co.id>
2019-08-03 21:07:05 +0700

    Added entity changesed

--- apps/hibiki/lib/hibiki/entity/schema.ex
+++ apps/hibiki/lib/hibiki/entity/schema.ex
@@ -1,11 +1,20 @@
 defmodule Hibiki.Entity.Schema do
   use Ecto.Schema
+  import Ecto.Changeset
 
   schema "entities" do
     field(:line_id, :string)
     field(:type, :string)
 
-    has_many(:created_tags, Hibiki.Schema.Tag, foreign_key: :creator_id)
-    has_many(:scope_tags, Hibiki.Schema.Tag, foreign_key: :scope_id)
+    has_many(:created_tags, Hibiki.Tag.Schema, foreign_key: :creator_id)
+    has_many(:scope_tags, Hibiki.Tag.Schema, foreign_key: :scope_id)
+  end
+
+  def changeset(struct, params) do
+    struct
+    |> cast(params, [:line_id, :type])
+    |> validate_required([:line_id, :type])
+    |> validate_inclusion(:type, ["global", "user", "group", "room"])
+    |> unique_constraint(:line_id)
   end
 end
