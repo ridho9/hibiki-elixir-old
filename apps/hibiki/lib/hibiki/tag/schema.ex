9914f5373b8a77d474dbf4ef18a5413759015e09
Ridho Pratama <p.ridho@yahoo.co.id>
2019-08-22 00:13:19 +0700

    Added tag schema changeset

--- apps/hibiki/lib/hibiki/tag/schema.ex
+++ apps/hibiki/lib/hibiki/tag/schema.ex
@@ -1,5 +1,6 @@
 defmodule Hibiki.Tag.Schema do
   use Ecto.Schema
+  import Ecto.Changeset
 
   schema "tags" do
     field(:name, :string)
@@ -10,5 +11,13 @@ defmodule Hibiki.Tag.Schema do
     belongs_to(:scope, Hibiki.Entity.Schema, foreign_key: :scope_id)
 
     timestamps()
+  end
+
+  def changeset(struct, params) do
+    struct
+    |> cast(params, [:name, :type, :value])
+    |> validate_required([:name, :type, :value, :creator, :scope])
+    |> validate_inclusion(:type, ["image", "text"])
+    |> unique_constraint(:name, name: :tags_scope_id_name_index)
   end
 end
