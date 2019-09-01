cd9b6d081df397d30aedbd5169579b24525f07f4
Ridho Pratama <p.ridho@yahoo.co.id>
2019-09-01 19:55:34 +0700

    ryn trim result

--- apps/hibiki/lib/hibiki/ryn/command.ex
+++ apps/hibiki/lib/hibiki/ryn/command.ex
@@ -13,8 +13,10 @@ defmodule Hibiki.Ryn.Command do
   def handle(%{"question" => question}, ctx) do
     answer = Enum.random(["yes", "no", "maybe"])
 
+    msg = String.trim("#{question}\n> #{answer}")
+
     ctx
-    |> add_text_message("#{question}\n   #{answer}")
+    |> add_text_message(msg)
     |> send_reply()
   end
 end
